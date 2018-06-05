20dbdc85782a3149ad0dcec04cf99815a889ff1d
“Anthony <nortonanthonya@gmail.com>
2018-06-05 09:10:12 -0600

    Add no duplicates assignment code

---
+++ exercises/no-duplicates/index.js
@@ -0,0 +1,12 @@
+const input = 'bodkkeeper larry';
+
+const results = input.split('').reduce((results, char) => {
+  if (!results.keep.includes(char) || char === ' ') {
+    results.keep += char;
+  } else {
+    results.dupeplicates += char;
+  }
+  return results
+}, {keep: '', dupeplicates: ''});
+
+console.log(results);
